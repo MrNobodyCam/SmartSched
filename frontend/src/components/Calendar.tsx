8cd01ef55ebf990159c79ac9630a06b1e8685354
iamkaizen <kimsokhom654@gmail.com>
2025-02-15 22:46:46 +0700

    This point push code to get support from sreangHour

---
+++ frontend/src/components/Calendar.tsx
@@ -0,0 +1,24 @@
+import Fullcalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import "@fullcalendar/common/main.css";
+
+function Calendar() {
+  return (
+    <div className="full-calendar-container">
+      <Fullcalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView={"dayGridMonth"}
+        headerToolbar={{
+          start: "today prev,next",
+          center: "title",
+          end: "dayGridMonth,timeGridWeek,timeGridDay",
+        }}
+      />
+    </div>
+  );
+}
+
+
+export default Calendar;
