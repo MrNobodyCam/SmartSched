e043e94eb8734c60014c8d1ff0e12e0b98675997
SamnangKong426 <samnangkong499@gmail.com>
2025-02-16 23:00:36 +0700

    Update migrations and other changes

--- backend/database/migrations/2025_02_16_000010_create_schedule_roadmaps_table.php
+++ backend/database/migrations/2025_02_16_000010_create_schedule_roadmaps_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('schedule_roadmaps', function (Blueprint $table) {
-            // $table->id();
+            $table->id();
             $table->unsignedBigInteger('schedule_id');
             $table->unsignedBigInteger('roadmap_id');
             $table->timestamps();
