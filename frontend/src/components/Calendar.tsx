d22c570e40cb7faab14ddbf0222af679b26315bc
iamkaizen <kimsokhom654@gmail.com>
2025-02-17 15:02:11 +0700

    Calendar V1.1

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -4,6 +4,8 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import "@fullcalendar/common/main.css";
 
+import "./components-styles/Calendar.css";
+
 function Calendar() {
   return (
     <div className="full-calendar-container">
@@ -15,10 +17,19 @@ function Calendar() {
           center: "title",
           end: "dayGridMonth,timeGridWeek,timeGridDay",
         }}
+        height="100%"
+        events={[
+          // ✅ Use "events" prop instead of "eventAdd"
+          {
+            id: "a",
+            title: "My Event",
+            start: "2025-02-18 10:00",
+            end: "2025-02-18 12:00",
+          },
+        ]}
       />
     </div>
   );
 }
-
 
 export default Calendar;
