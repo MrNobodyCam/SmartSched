9797fe0b6742b37bcd20ddaa33e9c154e39bec74
sereyvathna21 <vattdy192@gmail.com>
2025-02-17 21:30:44 +0700

    update on sidebar

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import calendarIcon from "../assets/icons/calendar.svg";
 import history from "../assets/icons/history.svg";
 import service from "../assets/icons/service.svg";
@@ -7,8 +8,15 @@ import setting from "../assets/icons/setting.svg";
 import logout from "../assets/icons/logout.svg";
 import "./Components-styles/sidebar.css";
 import NavBar from "./navbar";
+import "tailwindcss";
 
 function SideBar() {
+  const [selectedItem, setSelectedItem] = useState("calendar");
+
+  const handleItemClick = (item: React.SetStateAction<string>) => {
+    setSelectedItem(item);
+  };
+
   return (
     <>
       <NavBar />
@@ -16,11 +24,22 @@ function SideBar() {
         <nav>
           <p className="menu-label">MENU</p>
           <ul>
-            <li className="menu-item active">
+            <li
+              className={`menu-item ${
+                selectedItem === "calendar" ? "selected" : ""
+              }`}
+              onClick={() => handleItemClick("calendar")}
+            >
               <img src={calendarIcon} alt="calendar" />
               <span>Calendar</span>
             </li>
-            <li className="menu-item">
+            <br />
+            <li
+              className={`menu-item ${
+                selectedItem === "history" ? "selected" : ""
+              }`}
+              onClick={() => handleItemClick("history")}
+            >
               <img src={history} alt="history" />
               <span>History Schedule</span>
             </li>
@@ -28,23 +47,50 @@ function SideBar() {
         </nav>
         <div className="footer">
           <ul>
-            <li className="footer-item">
+            <li
+              className={`footer-item ${
+                selectedItem === "service" ? "selected" : ""
+              }`}
+              onClick={() => handleItemClick("service")}
+            >
               <img src={service} alt="service" />
               <span>Terms of Service</span>
             </li>
-            <li className="footer-item">
+            <br />
+            <li
+              className={`footer-item ${
+                selectedItem === "privacy" ? "selected" : ""
+              }`}
+              onClick={() => handleItemClick("privacy")}
+            >
               <img src={privacy} alt="privacy" />
               <span>Privacy Policy</span>
             </li>
-            <li className="footer-item">
+            <br />
+            <li
+              className={`footer-item ${
+                selectedItem === "contact" ? "selected" : ""
+              }`}
+              onClick={() => handleItemClick("contact")}
+            >
               <img src={contact} alt="contact" />
               <span>Contact Us</span>
             </li>
-            <li className="footer-item">
+            <br />
+            <li
+              className={`footer-item ${
+                selectedItem === "setting" ? "selected" : ""
+              }`}
+              onClick={() => handleItemClick("setting")}
+            >
               <img src={setting} alt="setting" />
               <span>Settings</span>
             </li>
-            <li className="log">
+            <br />
+            <li
+              className={`log ${selectedItem === "logout" ? "selected" : ""}`}
+              onClick={() => handleItemClick("logout")}
+            >
               <img src={logout} alt="logout" />
               <span className="text">Log out</span>
             </li>
