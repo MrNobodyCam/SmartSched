8d5121f70147cdbc6ddc9208cd8426e6f3421ac1
Netspecter <kaovichet11@gmail.com>
2025-02-26 23:30:10 +0700

    fix center

--- frontend/src/components/generate-schedule.tsx
+++ frontend/src/components/generate-schedule.tsx
@@ -7,7 +7,7 @@ const EmptySchedule = () => {
   const [viewType, setViewType] = useState("calendar");
 
   return (
-    <div className="w-full h-screen bg-white font-inter mx-auto shadow-md relative">
+    <div className="w-full h-screen flex flex-col bg-white font-inter mx-auto shadow-md">
       <div className="border-b border-gray-300 p-4">
         <div className="flex sm:flex-row flex-col p-4">
           <div className="flex flex-row items-center space-x-2 sm:mb-0 mb-4 justify-center">
@@ -43,15 +43,16 @@ const EmptySchedule = () => {
           <span className="text-sm">Calendar</span>
         </div>
       </div>
-
-      <div className="flex flex-col items-center justify-center max-w-lg mx-auto mt-[200px] sm:mt-[300px] px-4 text-center">
-        <div className="w-25 h-12 mb-4">
-          <img src={BoxIcon} alt="Empty box" className="max-w-full h-auto" />
+      <div className="w-full flex-grow flex items-center justify-center text-white">
+        <div className="flex flex-col items-center justify-center max-w-lg mx-auto px-4 text-center">
+          <div className="w-25 h-12 mb-4">
+            <img src={BoxIcon} alt="Empty box" className="max-w-full h-auto" />
+          </div>
+          <p className="text-gray-500 font-inter text-base">
+            No study schedule generated yet. Click 'Generate Schedule' to create
+            your personalized study plan!
+          </p>
         </div>
-        <p className="text-gray-500 font-inter text-base">
-          No study schedule generated yet. Click 'Generate Schedule' to create
-          your personalized study plan!
-        </p>
       </div>
     </div>
   );
