80ce8a226656300e2685fe9b302544d785a836d9
Netspecter <kaovichet11@gmail.com>
2025-02-26 23:56:19 +0700

    change button to have onclick

--- frontend/src/components/SecondaryBtn.tsx
+++ frontend/src/components/SecondaryBtn.tsx
@@ -5,9 +5,16 @@ interface Props {
   color?: string;
   border?: string;
   extraContent?: React.ReactNode;
+  onClick?: () => void;
 }
 
-const SecondaryBtn = ({ children, color, border, extraContent }: Props) => {
+const SecondaryBtn = ({
+  children,
+  color,
+  border,
+  extraContent,
+  onClick,
+}: Props) => {
   return (
     <button
       className="flex items-center font-medium text-sm md:text-base lg:text-lg py-2 md:py-3 lg:py-3 px-4 md:px-6 lg:px-8 rounded-lg shadow-md transition ease-out duration-300 cursor-pointer border-3 hover:opacity-50 hover:shadow-lg"
@@ -17,6 +24,7 @@ const SecondaryBtn = ({ children, color, border, extraContent }: Props) => {
           borderColor: border || "#27AE60",
         } as React.CSSProperties
       }
+      onClick={onClick}
     >
       {children}
       {extraContent && <span className="ml-2">{extraContent}</span>}
