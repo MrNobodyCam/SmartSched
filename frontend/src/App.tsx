03809401a7e9d275e9d0114db46f2a6f63cdd98d
Netspecter <kaovichet11@gmail.com>
2025-02-28 22:00:48 +0700

    ready button config login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+import Login from "./components/login";
 import PrimaryBtn from "./components/PrimaryBtn";
+
 function App() {
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+
   return (
     <>
-      <div className="flex justify-end">
-        <PrimaryBtn>Login</PrimaryBtn>
-      </div>
+      <PrimaryBtn
+        px="px-10 md:px-12 lg:px-4"
+        py="py-1"
+        onClick={() => setIsLoginOpen(true)}
+      >
+        Login
+      </PrimaryBtn>
+
+      {isLoginOpen && <Login onClose={() => setIsLoginOpen(false)} />}
     </>
   );
 }
--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -1,5 +1,13 @@
-function Login() {
-  return <></>;
-}
+import { X } from "lucide-react";
+
+const Login = ({ onClose }: { onClose: () => void }) => {
+  return (
+    <div className="fixed inset-0 bg-[#000000]/60 bg-opacity-50 backdrop-blur-sm flex items-center justify-center z-50 p-4">
+      <div className="flex justify-center bg-white p-5">
+        <h1 onClick={onClose}>Text</h1>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
