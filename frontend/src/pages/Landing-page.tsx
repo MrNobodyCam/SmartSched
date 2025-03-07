85cc41cf426cbce169b2b618567ae75fb7bb4147
Netspecter <kaovichet11@gmail.com>
2025-03-07 14:31:10 +0700

    ready

--- frontend/src/pages/Landing-page.tsx
+++ frontend/src/pages/Landing-page.tsx
@@ -6,44 +6,74 @@ import arrow from "../assets/icons/majesticons_arrow-up.svg";
 function Landingpage() {
   const [isOpen, setIsOpen] = useState(false); // State to manage dropdown toggle
   const teamMembers = [
-    { name: "Kao Vichet", position: "Backend Developer", imgSrc: "src/assets/images/Kao Vichet.jpg" },
-    { name: "Kim Sokhom", position: "Frontend Developer", imgSrc: "src/assets/images/profile.png" },
-    { name: "Kim Limkhun", position: "Frontend Developer", imgSrc: "src/assets/images/Kim Limkhun.JPG" },
-    { name: "Hy Sreanghour", position: "Frontend Developer", imgSrc: "src/assets/images/profile.png" },
-    { name: "Em Sereyvathna", position: "Frontend Developer", imgSrc: "src/assets/images/profile.png" },
-    { name: "Kong Samnang", position: "Backend Developer", imgSrc: "src/assets/images/profile.png" },
+    {
+      name: "Kao Vichet",
+      position: "Backend Developer",
+      imgSrc: "src/assets/images/Kao Vichet.jpg",
+    },
+    {
+      name: "Kim Sokhom",
+      position: "Frontend Developer",
+      imgSrc: "src/assets/images/profile.png",
+    },
+    {
+      name: "Kim Limkhun",
+      position: "Frontend Developer",
+      imgSrc: "src/assets/images/Kim Limkhun.JPG",
+    },
+    {
+      name: "Hy Sreanghour",
+      position: "Frontend Developer",
+      imgSrc: "src/assets/images/profile.png",
+    },
+    {
+      name: "Em Sereyvathna",
+      position: "Frontend Developer",
+      imgSrc: "src/assets/images/profile.png",
+    },
+    {
+      name: "Kong Samnang",
+      position: "Backend Developer",
+      imgSrc: "src/assets/images/profile.png",
+    },
   ];
 
   const keyFeatures = [
     {
       title: "Effortless Scheduling with AI",
-      description: "Tell us what you want to learn and when you're free—our AI will build the perfect schedule for you!",
+      description:
+        "Tell us what you want to learn and when you're free—our AI will build the perfect schedule for you!",
       bgColor: "#2D9CDB",
     },
     {
       title: "Smart Quizzes for Better Learning",
-      description: "Reinforce your learning with subject-specific quizzes! Our system provides quizzes tailored to the topics in your schedule, helping you review and track your progress after each session.",
+      description:
+        "Reinforce your learning with subject-specific quizzes! Our system provides quizzes tailored to the topics in your schedule, helping you review and track your progress after each session.",
       bgColor: "#27AE60",
     },
     {
       title: "Stay on Track with Notifications",
-      description: "Never miss a study session! Our system sends you timely notifications to remind you of your scheduled learning times, keeping you on track and organized.",
+      description:
+        "Never miss a study session! Our system sends you timely notifications to remind you of your scheduled learning times, keeping you on track and organized.",
       bgColor: "#F2994A",
     },
     {
       title: "Schedule History & Easy Access",
-      description: "Never lose track of your schedules! Our system automatically saves them for easy review anytime.",
+      description:
+        "Never lose track of your schedules! Our system automatically saves them for easy review anytime.",
       bgColor: "#EB5757",
     },
   ];
 
   return (
-    <div className="text-gray-900">
+    <div id="home" className="text-gray-900">
       {/* Header */}
-      <header className="bg-white shadow-md p-4 w-[100%]">
+      <header className="bg-white shadow-md p-4 w-[100%] fixed z-50">
         <div className="container mx-auto flex justify-between items-center">
           {/* Logo */}
-          <h1 className="text-3xl sm:text-3xl font-bold text-[#2D9CDB]">SmartSched</h1>
+          <h1 className="text-3xl sm:text-3xl font-bold text-[#2D9CDB]">
+            SmartSched
+          </h1>
 
           {/* Dropdown Toggle (Visible on Small Screens) */}
           <div className="sm:hidden relative">
@@ -60,25 +90,56 @@ function Landingpage() {
                 initial={{ opacity: 0, y: -20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.3 }}
-
                 className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2"
               >
-                <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-blue-50">Home</a>
-                <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-blue-50">Feature</a>
-                <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-blue-50">About</a>
-                <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-blue-50">Team</a>
-                <button className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-blue-50">Login</button>
+                <a
+                  href="#home"
+                  className="block px-4 py-2 text-gray-700 hover:bg-blue-50"
+                >
+                  Home
+                </a>
+                <a
+                  href="#feature"
+                  className="block px-4 py-2 text-gray-700 hover:bg-blue-50"
+                >
+                  Feature
+                </a>
+                <a
+                  href="#about"
+                  className="block px-4 py-2 text-gray-700 hover:bg-blue-50"
+                >
+                  About
+                </a>
+                <a
+                  href="#team"
+                  className="block px-4 py-2 text-gray-700 hover:bg-blue-50"
+                >
+                  Team
+                </a>
+                <button className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-blue-50">
+                  Login
+                </button>
               </motion.div>
             )}
           </div>
 
           {/* Navigation (Visible on Larger Screens) */}
           <nav className="hidden sm:flex items-center space-x-6">
-            <a href="#" className="text-gray-700 hover:text-[#2D9CDB]">Home</a>
-            <a href="#" className="text-gray-700 hover:text-[#2D9CDB]">Feature</a>
-            <a href="#" className="text-gray-700 hover:text-[#2D9CDB]">About</a>
-            <a href="#" className="text-gray-700 hover:text-[#2D9CDB]">Team</a>
-            <button className="bg-[#2D9CDB] text-white px-4 py-2 rounded-md hover:bg-[#2D9CDB]">Login</button>
+            <a href="#home" className="text-gray-700 hover:text-[#2D9CDB]">
+              Home
+            </a>
+            <a href="#feature" className="text-gray-700 hover:text-[#2D9CDB]">
+              Feature
+            </a>
+            <a href="#about" className="text-gray-700 hover:text-[#2D9CDB]">
+              About
+            </a>
+            <a href="#team" className="text-gray-700 hover:text-[#2D9CDB]">
+              Team
+            </a>
+            <button className="bg-[#2D9CDB] text-white px-4 py-2 rounded-md hover:bg-[#2D9CDB]">
+              Login
+            </button>
           </nav>
         </div>
       </header>
@@ -91,18 +152,22 @@ function Landingpage() {
         className="py-16 px-6 sm:px-16 flex flex-col sm:flex-row items-center justify-between"
       >
         {/* Left Text Section */}
-        <div className="sm:w-[50%] text-center sm:text-left space-y-8 sapce-x-8 items-center">
-          <h2 className="text-3xl sm:text-5xl text-gray-900 font-bold">Welcome to SmartSched</h2>
+        <div className="sm:w-[50%] text-center sm:text-left space-y-8 sapce-x-8 items-center mt-5">
+          <h2 className="text-3xl sm:text-5xl text-gray-900 font-bold">
+            Welcome to SmartSched
+          </h2>
           <p className="text-gray-700 text-[16px] mt-3 w-[380px]">
-            We are introducing our smart AI that can generate your everyday study. It can help you improve your study and your schedule.
+            We are introducing our smart AI that can generate your everyday
+            study. It can help you improve your study and your schedule.
           </p>
           <div className="mt-6">
-            <SecondaryBtn color="" border="">Get Started Now</SecondaryBtn>
+            <SecondaryBtn color="" border="">
+              Get Started Now
+            </SecondaryBtn>
           </div>
         </div>
-
         {/* Right Image Section */}
-        <div className="w-full sm:w-[50%] mt-8 sm:mt-0 flex justify-center sm:justify-end">
+        <div className="w-full sm:w-[50%] mt-8 sm:mt-5 flex justify-center sm:justify-end">
           <img
             className="w-full sm:w-auto object-contain"
             src="src/assets/images/Landing-page.png"
@@ -110,12 +175,13 @@ function Landingpage() {
           />
         </div>
       </motion.div>
-
       {/* Features Section */}
-      <section className="py-16 bg-[#A5A5A5]/20 -mt-10">
+      <section id="feature" className="py-16 bg-[#A5A5A5]/20 -mt-10">
         <div className="container mx-auto px-6 sm:px-20">
           {/* Section Title */}
-          <h3 className="text-3xl sm:text-5xl font-semibold text-center">Our Key Features</h3>
+          <h3 className="text-3xl sm:text-5xl font-semibold text-center">
+            Our Key Features
+          </h3>
 
           {/* Feature Cards */}
           <div className="mt-24 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-14 space-y-10">
@@ -147,14 +213,20 @@ function Landingpage() {
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
         className="py-16"
+        id="about"
       >
         <div className="container px-6 sm:px-20">
           {/* Section Title */}
           <div className="flex justify-center">
             <div className="w-full sm:w-[80%] ">
-              <h3 className="text-3xl sm:text-5xl text-gray-800 font-bold text-center">About Us</h3>
+              <h3 className="text-3xl sm:text-5xl text-gray-800 font-bold text-center">
+                About Us
+              </h3>
               <p className="text-gray-700 mt-4 sm:mt-12 text-center ">
-                Struggling to manage your study time? We’ve got you covered! SmartSched is an AI-powered tool that creates personalized study plans based on your free time, learning goals, and subject preferences.
+                Struggling to manage your study time? We’ve got you covered!
+                SmartSched is an AI-powered tool that creates personalized study
+                plans based on your free time, learning goals, and subject
+                preferences.
               </p>
             </div>
           </div>
@@ -172,15 +244,30 @@ function Landingpage() {
 
             {/* Right Section - Text */}
             <div className="flex flex-col justify-center text-[20px]">
-              <h2 className="text-2xl sm:text-3xl font-semibold text-gray-800 mb-4 sm:mb-6">Why Choose Us?</h2>
-              <ul className="list-disc list-inside text-gray-700 space-y-2 sm:space-y-4">
-                <li><strong>AI-Powered Study Schedules</strong> - Get the best study plan tailored to your needs.</li>
-                <li><strong>Smart Quizzes</strong> - Reinforce learning with interactive assessments.</li>
-                <li><strong>Schedule History</strong> - Never lose track of your schedules!</li>
-                <li><strong>Reminders & Alerts</strong> - Stay on track with study notifications.</li>
+              <h2 className="text-2xl sm:text-3xl font-semibold text-gray-800 mb-4 sm:mb-6">
+                Why Choose Us?
+              </h2>
+              <ul className="list-disc list-inside text-gray-700 space-y-2 sm:space-y-4 text-[16px]">
+                <li>
+                  <strong>AI-Powered Study Schedules</strong> - Get the best
+                  study plan tailored to your needs.
+                </li>
+                <li>
+                  <strong>Smart Quizzes</strong> - Reinforce learning with
+                  interactive assessments.
+                </li>
+                <li>
+                  <strong>Schedule History</strong> - Never lose track of your
+                  schedules!
+                </li>
+                <li>
+                  <strong>Reminders & Alerts</strong> - Stay on track with study
+                  notifications.
+                </li>
               </ul>
-              <p className="mt-4 sm:mt-6 text-red-500 font-semibold">
-                Join thousands of students using Smart Study Scheduler to learn more efficiently!
+              <p className="mt-4 sm:mt-6 text-red-500 font-semibold text-[16px]">
+                Join thousands of students using Smart Study Scheduler to learn
+                more efficiently!
               </p>
             </div>
           </div>
@@ -188,12 +275,16 @@ function Landingpage() {
       </motion.section>
 
       {/* Team Members */}
-      <section className="py-16 bg-[#A5A5A5]/20">
+      <section id="team" className="py-16 bg-[#A5A5A5]/20">
         <div className="container mx-auto px-6 sm:px-20">
           {/* Section Title */}
-          <h3 className="text-3xl sm:text-5xl text-gray-800 font-bold text-center">Meet Our Team</h3>
+          <h3 className="text-3xl sm:text-5xl text-gray-800 font-bold text-center">
+            Meet Our Team
+          </h3>
           <p className="text-gray-600 mt-4 max-w-2xl mx-auto text-center">
-            At SmartSched, we are dedicated to making learning more efficient with AI-powered study plans. Our developers team work together to create smart scheduling solutions that help students stay organized.
+            At SmartSched, we are dedicated to making learning more efficient
+            with AI-powered study plans. Our developers team work together to
+            create smart scheduling solutions that help students stay organized.
           </p>
 
           {/* Team Members Grid */}
@@ -204,9 +295,14 @@ function Landingpage() {
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: index * 0.2 }}
-                className="bg-white rounded-xl shadow-md p-6 flex flex-col items-center w-[100%] h-[250px] space-y-2">
+                className="bg-white rounded-xl shadow-md p-6 flex flex-col items-center w-[100%] h-[250px] space-y-2"
+              >
                 <div className="w-32 h-32 rounded-full overflow-hidden border-4 border-white -mt-12">
-                  <img src={member.imgSrc} alt={member.name} className="w-full h-full object-cover" />
+                  <img
+                    src={member.imgSrc}
+                    alt={member.name}
+                    className="w-full h-full object-cover"
+                  />
                 </div>
                 <h3 className="text-lg font-semibold mt-4">{member.name}</h3>
                 <p className="text-gray-500">{member.position}</p>
@@ -231,9 +327,21 @@ function Landingpage() {
             <div className="space-y-4">
               <h4 className="text-gray-400 font-bold">Support</h4>
               <ul className="space-y-2">
-                <li><a href="#" className="text-gray-400 hover:text-blue-500">Blog</a></li>
-                <li><a href="#" className="text-gray-400 hover:text-blue-500">Guides & Tutorials</a></li>
-                <li><a href="#" className="text-gray-400 hover:text-blue-500">Help Center</a></li>
+                <li>
+                  <a href="#" className="text-gray-400 hover:text-blue-500">
+                    Blog
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-gray-400 hover:text-blue-500">
+                    Guides & Tutorials
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-gray-400 hover:text-blue-500">
+                    Help Center
+                  </a>
+                </li>
               </ul>
             </div>
 
@@ -248,16 +356,34 @@ function Landingpage() {
               <h4 className="text-gray-400 font-bold">Stay Connected</h4>
               <ul className="space-y-2">
                 <li className="flex items-center space-x-2">
-                  <img src="src/assets/icons/Facebook - Negative.svg" alt="Facebook" className="w-6 h-6" />
-                  <a href="a" className="text-gray-400 hover:text-blue-500">wwe.smartsched@facebook.com</a>
+                  <img
+                    src="src/assets/icons/Facebook - Negative.svg"
+                    alt="Facebook"
+                    className="w-6 h-6"
+                  />
+                  <a href="a" className="text-gray-400 hover:text-blue-500">
+                    www.smartsched@facebook.com
+                  </a>
                 </li>
                 <li className="flex items-center space-x-2">
-                  <img src="src/assets/icons/Twitter - Negative.svg" alt="Twitter" className="w-6 h-6" />
-                  <a href="#" className="text-gray-400 hover:text-blue-500">wwe.smartsched@twitter.com</a>
+                  <img
+                    src="src/assets/icons/Twitter - Negative.svg"
+                    alt="Twitter"
+                    className="w-6 h-6"
+                  />
+                  <a href="#" className="text-gray-400 hover:text-blue-500">
+                    www.smartsched@twitter.com
+                  </a>
                 </li>
                 <li className="flex items-center space-x-2">
-                  <img src="src/assets/icons/google.svg" alt="Google" className="w-6 h-6" />
-                  <a href="#" className="text-gray-400 hover:text-blue-500">wwe.smartsched@google.com</a>
+                  <img
+                    src="src/assets/icons/google.svg"
+                    alt="Google"
+                    className="w-6 h-6"
+                  />
+                  <a href="#" className="text-gray-400 hover:text-blue-500">
+                    www.smartsched@google.com
+                  </a>
                 </li>
               </ul>
             </div>
@@ -272,9 +398,11 @@ function Landingpage() {
             <div className="space-y-4 ">
               <h4 className="text-gray-400 font-bold">Get in Touch</h4>
               <ul className="space-y-2">
-                <li className="text-gray-400">+855 77 818 040</li>
+                <li className="text-gray-400">+855 77 623 512</li>
                 <li className="text-gray-400">smartsched@gmail.com</li>
-                <li className="text-gray-400">Street 6A, Prek Leab, CADT, Phnom Penh</li>
+                <li className="text-gray-400">
+                  Street 6A, Prek Leab, CADT, Phnom Penh
+                </li>
               </ul>
             </div>
           </div>
