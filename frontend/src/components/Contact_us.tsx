bc8d50fcf7a96d42ad688bc9b8f71f078a97c11d
Sreanghourhy <sreanghour.hygeneral007@gmail.com>
2025-03-12 15:45:40 +0700

    hi

--- frontend/src/components/Contact_us.tsx
+++ frontend/src/components/Contact_us.tsx
@@ -29,15 +29,22 @@ const FullScreenContactForm = () => {
   };
 
   return (
-    <div className="flex items-start justify-start p-4 sm:p-12 w-full h-full" style={{ position: 'absolute' }}>
+    <div className="flex items-start justify-start p-4 sm:p-12 w-full h-full">
       <div className="w-[876px] h-full p-6 rounded-md">
         <form onSubmit={handleSubmit}>
           <div className="mb-5">
-            <h1 className="text-2xl sm:text-4xl font-medium mb-1">Contact us</h1>
-            <p className="text-base sm:text-lg text-gray-400 mb-6">Reach out and we'll get in touch within 24 hours.</p>
+            <h1 className="text-2xl sm:text-4xl font-medium mb-1">
+              Contact us
+            </h1>
+            <p className="text-base sm:text-lg text-gray-400 mb-6">
+              Reach out and we'll get in touch within 24 hours.
+            </p>
           </div>
           <div className="mb-5">
-            <label htmlFor="title" className="block text-base sm:text-lg font-bold text-black mb-1">
+            <label
+              htmlFor="title"
+              className="block text-base sm:text-lg font-bold text-black mb-1"
+            >
               Title
             </label>
             <input
@@ -52,7 +59,10 @@ const FullScreenContactForm = () => {
             />
           </div>
           <div className="mb-5">
-            <label htmlFor="message" className="mb-3 block text-base sm:text-lg font-bold text-black">
+            <label
+              htmlFor="message"
+              className="mb-3 block text-base sm:text-lg font-bold text-black"
+            >
               Message
             </label>
             <textarea
@@ -74,18 +84,28 @@ const FullScreenContactForm = () => {
               checked={formData.agreedToPolicy}
               onChange={handleCheckboxChange}
               className="mr-2"
-              style={{ width: '18px', height: '18px' }}
+              style={{ width: "18px", height: "18px" }}
               required
             />
-            <label htmlFor="policy" className="text-base sm:text-lg text-gray-600 mt-2 sm:mt-0">
-              You agree to our friendly <a href="/privacy-policy" className="text-blue-500 hover:underline">privacy policy</a>.
+            <label
+              htmlFor="policy"
+              className="text-base sm:text-lg text-gray-600 mt-2 sm:mt-0"
+            >
+              You agree to our friendly{" "}
+              <a
+                href="/privacy-policy"
+                className="text-blue-500 hover:underline"
+              >
+                privacy policy
+              </a>
+              .
             </label>
           </div>
           <div>
             <button
               type="submit"
               className="hover:shadow-form rounded-md bg-[#2D9CDB] py-3 px-8 text-base sm:text-lg font-bold text-white outline-none"
-              style={{ borderRadius: '12px' }}
+              style={{ borderRadius: "12px" }}
             >
               Sent Message
             </button>
