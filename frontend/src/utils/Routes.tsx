ab3880a728ceef77a0cdfe362c12d2a353cfc182
Kao Vichet <kaovichet11@gmail.com>
2025-03-14 18:29:12 +0700

    ready history schedult

--- frontend/src/utils/Routes.tsx
+++ frontend/src/utils/Routes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import CustomCalendar from "../components/Schedule_create/Calendar";
 import Landingpage from "../pages/Landing-page";
 import "../index.css";
+import HistoryCustomCalendar from "../components/history-schedule/History_Calendar";
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,8 @@ const AppRoutes = () => {
       {/* <Route path="/generate-schedule" element={<ScheduleListview />} /> */}
       <Route path="/generate-schedule/calendar" element={<CustomCalendar />} />
       <Route path="/generate-schedule/listview" element={<CustomCalendar />} />
+      <Route path="/history/calendar" element={<HistoryCustomCalendar />} />
+      <Route path="/history/listview" element={<HistoryCustomCalendar />} />
       <Route path="/" element={<Landingpage />} />
     </Routes>
   );
